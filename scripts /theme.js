6025f5f9100b685e9ddfc3c2bdaa7d3c16f14d66
JeffOgah <jeff.ogah@gmail.com>
2019-12-14 03:58:06 +0100

    renamed assets to images

--- scripts /theme.js
+++ scripts /theme.js
@@ -5,14 +5,14 @@ if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
   if (currentTheme === "dark") {
     toggleSwitch.checked = true;
-    toggleSwitch.src = `../assets/sun.svg`;
+    toggleSwitch.src = `../images/sun.svg`;
   }
   else {
-    toggleSwitch.src = `../assets/moon.svg`;
+    toggleSwitch.src = `../images/moon.svg`;
   }
 }
 else {
-  toggleSwitch.src = `../assets/sun.svg`;
+  toggleSwitch.src = `../images/sun.svg`;
 }
 
 toggleSwitch.addEventListener(
@@ -21,12 +21,12 @@ toggleSwitch.addEventListener(
     console.log(event.target.checked)
     if (event.target.checked) {
       document.documentElement.setAttribute("data-theme", "light");
-      toggleSwitch.src = `../assets/moon.svg`;
+      toggleSwitch.src = `../images/moon.svg`;
       localStorage.setItem("theme", "light");
       event.target.checked = false;
     } else {
       document.documentElement.setAttribute("data-theme", "dark");
-      toggleSwitch.src = `../assets/sun.svg`;
+      toggleSwitch.src = `../images/sun.svg`;
       localStorage.setItem("theme", "dark");
       event.target.checked = true;
     }
